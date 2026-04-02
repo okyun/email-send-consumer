299fccea1209db5952969485d884ffd584e0406a
okyun <dhrdbs8066@naver.com>
2026-04-02 19:41:41 +0900

    오류 일부로 발생

--- src/main/java/org/example/emailsendconsumer/EmailSendConsumer.java
+++ src/main/java/org/example/emailsendconsumer/EmailSendConsumer.java
@@ -10,16 +10,17 @@ public class EmailSendConsumer {
             topics = "email.send",
             groupId = "email-send-group" // 컨슈머 그룹 이름(없으면 새로 컨슈머 그룹 만듬)
     )
+    
     public void consume(String message) {
         System.out.println("Kafka로부터 받아온 메시지: " + message);
 
         EmailSendMessage emailSendMessage = EmailSendMessage.fromJson(message);
 
         // 잘못된 이메일 주소일 경우 실패 가정
-//        if (emailSendMessage.getTo().equals("fail@naver.com")) {
-//            System.out.println("잘못된 이메일 주소로 인해 발송 실패");
-//            throw new RuntimeException("잘못된 이메일 주소로 인해 발송 실패");
-//        }
+        if (emailSendMessage.getTo().equals("fail@naver.com")) {
+            System.out.println("잘못된 이메일 주소로 인해 발송 실패");
+            throw new RuntimeException("잘못된 이메일 주소로 인해 발송 실패");
+        }
         // ... 실제 이메일 발송 로직은 생략 ...
         try {
             Thread.sleep(3000); // 이메일 발송을 하는 데 3초가 걸린다고 가정!!
